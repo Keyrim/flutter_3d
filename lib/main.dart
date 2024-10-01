275338ee6b414b76fb0872de91f10bf5fd9f96ad
Théo Magne <theo.magne.fr@gmail.com>
2024-10-01 23:30:27 +0200

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_3d/main_view.dart';
-import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 void main() {
   runApp(const MainApp());
